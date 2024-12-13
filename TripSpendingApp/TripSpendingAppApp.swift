03ef4ab3b376ce63c986677bdb75b4ee483e9d03
PFGrande <2002.pedrogrande@gmail.com>
2024-12-13 15:33:45 -0800

    imported firebase auth packages

--- TripSpendingApp/TripSpendingAppApp.swift
+++ TripSpendingApp/TripSpendingAppApp.swift
@@ -6,9 +6,32 @@
 //
 
 import SwiftUI
+//import FirebaseAuth
+import FirebaseCore
+
+// https://console.firebase.google.com/u/0/project/tripspendingapp/overview
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+  func application(_ application: UIApplication,
+
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+    FirebaseApp.configure()
+
+    return true
+
+  }
+
+}
+
 
 @main
 struct TripSpendingAppApp: App {
+    // register app delegate for Firebase setup
+
+      @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    
     var body: some Scene {
         WindowGroup {
             LoginView()
