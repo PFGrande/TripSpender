ea4b78a6df1260a59cca156ed320fc4e16a1ca7f
PFGrande <2002.pedrogrande@gmail.com>
2024-12-12 17:50:15 -0800

    Initial Commit

---
+++ TripSpendingApp/TripSpendingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripSpendingAppApp.swift
+//  TripSpendingApp
+//
+//  Created by Pedro F. Grande on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TripSpendingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
