d42d22a7f7eb876469363240d0ea98b0d1923638
PFGrande <2002.pedrogrande@gmail.com>
2024-12-13 01:26:50 -0800

    added ui elementsin loginview

--- TripSpendingApp/LoginView.swift
+++ TripSpendingApp/LoginView.swift
@@ -10,29 +10,69 @@ import SwiftUI
 struct LoginView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack {
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(.accentColor)
+                
+                Button(
+                    action: {
+                        // did tap
+                    },
+                    label: {
+                        Text("Log in")
+                            .padding()
+                            .frame(width: 300, height: 80)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(20)
+                            .shadow(color: .gray, radius: 5, x: 0, y: 5)
+                    }
+                )
+                
+                Button(
+                    action: {
+                        // did tap
+                    },
+                    label: {
+                        Text("Register")
+                            .padding()
+                            .frame(width: 300, height: 80)
+                            .background(Color.orange)
+                            .foregroundColor(.white)
+                            .cornerRadius(20)
+                            .shadow(color: .gray, radius: 5, x: 0, y: 5)
+                    }
+                )
+                
+                Button(
+                    action: {
+                        // did tap
+                    },
+                    label: {
+                        Text("Offline Mode")
+                            .padding()
+                            .frame(width: 300, height: 80)
+                            .background(Color.red)
+                            .foregroundColor(.white)
+                            .cornerRadius(20)
+                            .shadow(color: .gray, radius: 5, x: 0, y: 5)
+                    }
+                )
+            }
             
-            Button(
-                action: {
-                    // did tap
-                },
-                label: { Text("Log in") }
-            )
-            Button(
-                action: {
-                    // did tap
-                },
-                label: { Text("Register") }
-            )
-            Button(
-                action: {
-                    // did tap
-                },
-                label: { Text("Offline Mode") }
-            )
+            
+            HStack {
+                Image("spendee")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Image("spender")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            .frame(height: 200)
         }
         .padding()
     }
