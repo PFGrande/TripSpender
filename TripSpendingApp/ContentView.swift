195cb3817f094b53548032354360fe04e7dc5346
PFGrande <2002.pedrogrande@gmail.com>
2024-12-14 19:50:36 -0800

    gave files mode fitting names

--- TripSpendingApp/ContentView.swift
+++ TripSpendingApp/ContentView.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftUI
+import FirebaseAuth
 
 
 // code from lecture https://www.youtube.com/watch?v=g4HoFkd_I2Q&list=PLPNCkYpwTr1SKK8zrqRBJC71c4MO9yTIM&index=10
@@ -14,9 +15,13 @@ struct ContentView: View {
     
     @State
     var path: [Route] = []
+    @State
+    private var isLoggedIn: Bool = Auth.auth().currentUser != nil // take user directly to tripview if logged in
+    // add this feature later, focus on sending and retrieving items
     
     var body: some View {
         NavigationStack(path: $path) { // THIS IS THE ON NAVIGATION FUNCTION WE PASS INTO HOMEVIEW
+            
             HomeView { destination in
                 path.append(destination)
             }.navigationDestination(for: Route.self) { route in
