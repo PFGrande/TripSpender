97619e7e9ed861321a6a7cbfcc5f6ced877aa6e8
PFGrande <2002.pedrogrande@gmail.com>
2024-12-16 04:15:35 -0800

    fixed bug where items would always fail

--- TripSpendingApp/Destinations/AddItemView.swift
+++ TripSpendingApp/Destinations/AddItemView.swift
@@ -28,6 +28,9 @@ struct AddItemView: View {
         VStack {
             HStack {
                 TextField("Item Name", text: $itemName)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                // formatting for price field
                 HStack {
                     Text("$")
                     TextField("Price", text: $itemPriceString)
@@ -35,7 +38,7 @@ struct AddItemView: View {
                         .onChange(of: itemPriceString) { newValue in
                             
                             
-                            
+                            // make it so that . is always 2 spaces away from the end
                             if (newValue.count > 3) {
 //                                var endingIndex = newValue.index(newValue.endIndex, offsetBy: -2)
                                 if let periodIndex = itemPriceString.firstIndex(of: ".") {
@@ -45,57 +48,9 @@ struct AddItemView: View {
                                 let endingIndex = newValue.index(newValue.endIndex, offsetBy: -3)
                                 itemPriceString.insert(".", at: endingIndex)
                                 
-                                
-//                                if (!newValue.contains(".")){
-//                                    let endingIndex = newValue.index(newValue.endIndex, offsetBy: -2)
-//                                    itemPriceString.insert(".", at: endingIndex)
-                                    
-//                                }
-                                
-//                                if (newValue.contains(".") && newValue.count > 4) {
-////                                    itemPriceString = String(newValue.reversed())
-//                                    var endingIndex = newValue.index(ofAccessibilityElement: newValue.firstIndex(of: "."))
-//                                    itemPriceString.remove(at: endingIndex)
-//                                }
-                                
-                                
-                                
-                                
-                                
                             }
                             
-                            
-//                            if (itemPriceString.count >= 4) {
-//                                let removeIndex = itemPriceString.index(after: itemPriceString.endIndex, offsetBy: 4, limitedBy: itemPriceString.endIndex)
-//                                itemPriceString.remove(at: removeIndex)
-//
-//                                let insertIndex = itemPriceString.index(after: itemPriceString.endIndex, offsetBy: 3, limitedBy: itemPriceString.endIndex)
-//                                itemPriceString.insert(".", at: insertIndex)
-//                            }
-                            
-                            
-                            // guarantee here (using the string) that the double will not
-                            // exceed to decimal places
-                            
-                            
 
-                            
-//                            print("NEWVALUE VALUE::::")
-//                            print(newValue)
-//
-//
-//                            if let price = Double(newValue) {// Convert the string to a Double
-//                                if (price != 0) {
-//                                    itemPrice = (price * 100).rounded() / 100
-//                                    itemPriceString = formatPrice(itemPrice)
-//                                }
-//
-//
-//                            } else {
-//                                errorMessage = "invalid price, please try again"
-//                                itemPrice = 0
-//
-//                            }
                         }
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                 }
@@ -104,6 +59,11 @@ struct AddItemView: View {
             
             Button(
                 action: {
+                    if let price = Double(itemPriceString) {
+                        itemPrice = price
+                    } else {
+                        itemPrice = 0
+                    }
                     //                UnitTemperature cool info
                     if itemName.isEmpty || itemPrice <= 0 {
                         errorMessage = "Please provide a valid item name and price."
